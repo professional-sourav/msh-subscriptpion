a7045d817d017608a082cf20723b6028d7936621
Sourav <ouravthedevguy@gmail.com>
2022-04-20 08:15:12 +0530

    Added tag crud and few changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use \Backpack\CRUD\app\Models\Traits\CrudTrait;
     use HasApiTokens, HasFactory, Notifiable, Billable;
 
     /**
